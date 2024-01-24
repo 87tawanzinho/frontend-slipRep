539cd92dfd77836ff90cc1ed5b86b46093c3fa06
my <muem@ex.com>
2024-01-24 11:25:59 -0300

    ..

--- app/(pagesAfterLogin)/emotion/page-wrapper-up.tsx
+++ app/(pagesAfterLogin)/emotion/page-wrapper-up.tsx
@@ -8,10 +8,10 @@ export const PageWrapperUp = ({ children }: { children: React.ReactNode }) => {
       <AnimatePresence>
         <motion.div
           className="w-full"
-          initial={{ opacity: 0, y: 100 }}
+          initial={{ opacity: 0, y: 20 }}
           animate={{ opacity: 1, y: 0 }}
-          exit={{ opacity: 0, y: -100 }}
-          transition={{ type: "tween", duration: 0.4 }}
+          exit={{ opacity: 0, y: -20 }}
+          transition={{ type: "just", duration: 0.2 }}
         >
           {children}
         </motion.div>
