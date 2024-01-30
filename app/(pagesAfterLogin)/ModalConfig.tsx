41157dbca709abca3df7f4a9462ab35dbd366cbf
my <muem@ex.com>
2024-01-30 09:34:45 -0300

    ..'

--- app/(pagesAfterLogin)/ModalConfig.tsx
+++ app/(pagesAfterLogin)/ModalConfig.tsx
@@ -6,19 +6,39 @@ import Loading from "./loading";
 import { changePaidBill } from "./datas/BillFunctions/paidBill";
 import { removeBill } from "./datas/BillFunctions/removeBill";
 import { getClickedBill } from "./datas/BillFunctions/clickedOnGear";
+import { PageWrapper } from "./emotion/page-wrapper";
+import { PageWrapperUp } from "./emotion/page-wrapper-up";
 
 interface typeConfig {
   type: "Bill" | "Slip";
   setConfigModal: Dispatch<SetStateAction<boolean>>;
   allBillsData: Dispatch<SetStateAction<myBills[]>>;
 }
+
+interface interestAndDate {
+  interest: number;
+  date: string;
+}
 function ModalConfig({ type, setConfigModal, allBillsData }: typeConfig) {
   const [data, setData] = useState<myBills | null>();
+  const [showDivInterestRate, setShowDivInterestRate] = useState(false);
+  const [inputsToConfirm, setInputsToConfirm] = useState<interestAndDate>();
   if (type === "Bill") {
     useEffect(() => {
       const clickedBill = getClickedBill();
       setData(clickedBill);
     }, [data]);
+
+    const handleValueOfInputs = (e: React.ChangeEvent<HTMLInputElement>) => {
+      const { name, value } = e.target;
+
+      setInputsToConfirm((prev: any) => ({
+        ...prev,
+        [name]: value,
+      }));
+
+      console.log(name, value);
+    };
 
     return (
       <>
@@ -46,28 +66,34 @@ function ModalConfig({ type, setConfigModal, allBillsData }: typeConfig) {
               </div>
 
               <div className="mt-4 flex gap-2 items-center">
-                <button
-                  onClick={() => {
-                    setConfigModal(false);
-                    changePaidBill(
-                      data._id,
-                      fetchDataAndSetBills,
-                      allBillsData
-                    );
-                  }}
-                  className={`${
-                    data.paid
-                      ? "bg-red-600 text-white"
-                      : "bg-green-600 text-white"
-                  }
-                  text-sm
-                 
-                      rounded h-8 hover:opacity-75 w-48
-                  `}
-                >
-                  {data.paid ? "NÃO PAGUEI" : "PAGAR DESPESA"}
-                </button>
+                {data.paid && (
+                  <button
+                    onClick={() => {
+                      setConfigModal(false);
+                      changePaidBill(
+                        data._id,
+                        fetchDataAndSetBills,
+                        allBillsData
+                      );
+                    }}
+                    className="bg-red-600 text-white h-8 w-48 rounded hover:opacity-75"
+                  >
+                    REMOVER DESPESA
+                  </button>
+                )}
 
+                {!data.paid && (
+                  <button
+                    onClick={() => setShowDivInterestRate(!showDivInterestRate)}
+                    className={`h-8 w-48 rounded hover:opacity-75  ${
+                      showDivInterestRate
+                        ? "bg-gray-300 border text-black"
+                        : "bg-green-600 text-white "
+                    } `}
+                  >
+                    {!showDivInterestRate ? "PAGAR DESPESA" : "VOLTAR"}
+                  </button>
+                )}
                 <button
                   className="bg-black text-white h-8 w-48 rounded hover:bg-opacity-75"
                   onClick={() => {
@@ -78,6 +104,40 @@ function ModalConfig({ type, setConfigModal, allBillsData }: typeConfig) {
                   REMOVER DESPESA
                 </button>
               </div>
+              {showDivInterestRate && (
+                <PageWrapperUp>
+                  <div className="mt-4">
+                    <p className="">Valor do Juros</p>
+                    <input
+                      type="number"
+                      name="interest"
+                      defaultValue={0}
+                      onChange={handleValueOfInputs}
+                    />
+                    <p className="mt-2">Data do Pagamento</p>
+                    <div className="flex gap-2">
+                      <input
+                        type="date"
+                        name="date"
+                        onChange={handleValueOfInputs}
+                      />
+                      <button
+                        className="bg-green-800 text-white rounded w-32 hover:opacity-75"
+                        onClick={() => {
+                          setConfigModal(false);
+                          changePaidBill(
+                            data._id,
+                            fetchDataAndSetBills,
+                            allBillsData
+                          );
+                        }}
+                      >
+                        Pagar
+                      </button>
+                    </div>
+                  </div>
+                </PageWrapperUp>
+              )}
             </div>
           </div>
         ) : (
