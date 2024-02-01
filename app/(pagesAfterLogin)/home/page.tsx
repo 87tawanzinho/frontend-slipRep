a650ef59fceb7b5c1996f6b48dad9e3d0f8b81b3
87tawanzinho <87tawanzinho@gmail.com>
2024-02-01 17:27:37 -0300

    ..

--- app/(pagesAfterLogin)/home/page.tsx
+++ app/(pagesAfterLogin)/home/page.tsx
@@ -146,10 +146,10 @@ function PageHome() {
               type="Bills"
               thereBillsToPayToday={thereBillsToPayToday}
               payToday={
-                <div className="rounded-lg mb-4  shadow pb-4 z-0 bg-zinc-800 text-zinc-200">
+                <div className="rounded-lg mb-4  shadow pb-4 z-0  text-white">
                   <p className="flex gap-2 items-center mb-4">
                     {" "}
-                    <CiWarning size={32} className="text-zinc-200" /> Contas para
+                    <CiWarning size={32} className="text-white" /> Contas para
                     pagar hoje
                   </p>
 
@@ -158,7 +158,7 @@ function PageHome() {
                       {isToday(parseISO(bill.date)) ? (
                         <PageWrapper>
                           <div
-                            className={` flex  text-[14px] justify-between items-center text-zinc-200 px-2 `}
+                            className={` flex  text-[14px] justify-between items-center text-white px-2 `}
                           >
                             <div
                               className={` ${
