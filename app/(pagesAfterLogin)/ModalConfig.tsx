13a0946619ef58432f4754d0dca5d395be232686
my <muem@ex.com>
2024-02-04 10:40:37 -0300

    ..

--- app/(pagesAfterLogin)/ModalConfig.tsx
+++ app/(pagesAfterLogin)/ModalConfig.tsx
@@ -9,6 +9,11 @@ import { getClickedBill } from "./datas/BillFunctions/clickedOnGear";
 import { PageWrapper } from "./emotion/page-wrapper";
 import { PageWrapperUp } from "./emotion/page-wrapper-up";
 import { useHide } from "../context/HideDivContext";
+import { CiEdit } from "react-icons/ci";
+import { TextField } from "@radix-ui/themes";
+import { instance } from "../axios/instance";
+import { MdOutlineDone } from "react-icons/md";
+import { LiaUndoAltSolid } from "react-icons/lia";
 
 interface typeConfig {
   type: "Bill" | "Slip";
@@ -24,6 +29,8 @@ function ModalConfig({ type, setConfigModal, allBillsData }: typeConfig) {
   const [data, setData] = useState<myBills | null>();
   const [showDivInterestRate, setShowDivInterestRate] = useState(false);
   const [inputsToConfirm, setInputsToConfirm] = useState<interestAndDate>();
+  const [newName, setNewName] = useState(false);
+  const [newNameOfData, setNewNameOfData] = useState("");
   const { setHide } = useHide();
   if (type === "Bill") {
     useEffect(() => {
@@ -40,6 +47,20 @@ function ModalConfig({ type, setConfigModal, allBillsData }: typeConfig) {
       }));
 
       console.log(name, value);
+    };
+
+    const updateName = async () => {
+      try {
+        await instance.put("updateNameOfBill", {
+          userName: localStorage.getItem("name"),
+          id: data?._id,
+          name: newNameOfData,
+        });
+
+        setConfigModal(false);
+      } catch (error) {
+        console.log(error);
+      }
     };
 
     return (
@@ -62,57 +83,104 @@ function ModalConfig({ type, setConfigModal, allBillsData }: typeConfig) {
                 </p>
               </div>
               <div className="py-2 px-4">
-                <p>Despesa: {data?.name} </p>
-                <p>
-                  Status Atual:
-                  <span
-                    className={data.paid ? "text-emerald-800 " : "text-red-800"}
-                  >
-                    {" "}
-                    {data.paid ? "Pago" : "Não pago"}
-                  </span>{" "}
-                </p>
+                <div className="flex items-center gap-2">
+                  {!newName ? (
+                    <div className="flex items-center gap-2">
+                      <p>Despesa: {data?.name} </p>
+                      <CiEdit
+                        onClick={() => setNewName(true)}
+                        className="hover:cursor-pointer hover:opacity-75"
+                        size={20}
+                      />
+                    </div>
+                  ) : (
+                    <div className="flex flex-col gap-2 py-4">
+                      <h2>Defina um novo nome</h2>
+
+                      <div className="flex items-center gap-2">
+                        <TextField.Input
+                          size={"2"}
+                          defaultValue={data.name}
+                          onChange={(e) => setNewNameOfData(e.target.value)}
+                        />
+
+                        <LiaUndoAltSolid
+                          size={24}
+                          onClick={() => setNewName(false)}
+                          className="hover:cursor-pointer hover:opacity-75 bg-red-400 rounded-full p-1 text-white"
+                        />
+                        <button
+                          disabled={newNameOfData === ""}
+                          className={`${newNameOfData === "" && "opacity-50"}`}
+                        >
+                          <MdOutlineDone
+                            className="hover:cursor-pointer hover:opacity-75 bg-emerald-400 rounded-full p-1 text-white"
+                            size={24}
+                            onClick={updateName}
+                          />
+                        </button>
+                      </div>
+                    </div>
+                  )}
+                </div>
+                {!newName && (
+                  <p>
+                    Status Atual:
+                    <span
+                      className={
+                        data.paid ? "text-emerald-800 " : "text-red-800"
+                      }
+                    >
+                      {" "}
+                      {data.paid ? "Pago" : "Não pago"}
+                    </span>{" "}
+                  </p>
+                )}
               </div>
 
-              <div className="flex gap-2 items-center py-4 px-4">
-                {data.paid && (
+              {!newName && (
+                <div className="flex gap-2 items-center py-4 px-4">
+                  {data.paid && (
+                    <button
+                      onClick={() => {
+                        setConfigModal(false);
+                        changePaidBill(
+                          data._id,
+                          fetchDataAndSetBills,
+                          allBillsData
+                        );
+                      }}
+                      className="bg-red-600 text-white h-8 w-48 rounded hover:opacity-75"
+                    >
+                      Desfazer Pagamento
+                    </button>
+                  )}
+
+                  {!data.paid && (
+                    <button
+                      onClick={() =>
+                        setShowDivInterestRate(!showDivInterestRate)
+                      }
+                      className={`h-8 w-48 rounded hover:opacity-75  ${
+                        showDivInterestRate
+                          ? "bg-gray-300 border text-gray-800"
+                          : "bg-emerald-600 text-white "
+                      } `}
+                    >
+                      {!showDivInterestRate ? "Pagar Despesa" : "Voltar"}
+                    </button>
+                  )}
                   <button
+                    className="bg-black text-white h-8 w-48 rounded hover:bg-opacity-75"
                     onClick={() => {
                       setConfigModal(false);
-                      changePaidBill(
-                        data._id,
-                        fetchDataAndSetBills,
-                        allBillsData
-                      );
+                      removeBill(data._id, fetchDataAndSetBills, data, setData);
                     }}
-                    className="bg-red-600 text-white h-8 w-48 rounded hover:opacity-75"
                   >
-                    REMOVER PAGAMENTO
+                    Remover Pagamento
                   </button>
-                )}
-
-                {!data.paid && (
-                  <button
-                    onClick={() => setShowDivInterestRate(!showDivInterestRate)}
-                    className={`h-8 w-48 rounded hover:opacity-75  ${
-                      showDivInterestRate
-                        ? "bg-gray-300 border text-gray-800"
-                        : "bg-emerald-600 text-white "
-                    } `}
-                  >
-                    {!showDivInterestRate ? "PAGAR DESPESA" : "VOLTAR"}
-                  </button>
-                )}
-                <button
-                  className="bg-black text-white h-8 w-48 rounded hover:bg-opacity-75"
-                  onClick={() => {
-                    setConfigModal(false);
-                    removeBill(data._id, fetchDataAndSetBills, data, setData);
-                  }}
-                >
-                  REMOVER DESPESA
-                </button>
-              </div>
+                </div>
+              )}
               {showDivInterestRate && (
                 <PageWrapperUp>
                   <div className=" py-4 px-4 bg-emerald-950 text-white">
